6c0380e40a1f9854e0f66ecb4554541c246e3178
wanting <wantingpeng.cici@gmail>
2024-06-19 09:12:02 +0200

    compare  keydown input keyup change event
    When the keydown event fires, the key has been pressed The new character
    has not yet been added to the input's value when the keydown event is triggered.

---
+++ Type Ahead/test.js
@@ -0,0 +1,9 @@
+
+const data = [
+  { name: 'Alice', city: 'Boston' },
+  { name: 'Bob', city: 'New York' },
+  { name: 'Charlie', city: 'Bosnia' },
+  { name: 'David', city: 'Los Angeles' }
+];
+
+console.log(data.map(object=>object.name).join(''));
