a99b53d46606667250a41b58feda851735c4c72c
wanting <wantingpeng.cici@gmail>
2024-06-22 23:51:02 +0200

    discard the above method
    fece the reset problem again because now in LocalStorage there are only text.value no html,
    but we cannot just  Arts.innerHTML=html; bacause func xx will not be loaded when call func initializeList

--- LocalStorage/xx.js
+++ LocalStorage/xx.js
@@ -7,16 +7,27 @@ const reset= document.querySelector('#resetButton')
 
 
 //console.log(text)
-addItems.addEventListener('submit',submit)
-
+addItems.addEventListener('submit',xx)
+Arts.addEventListener('click',check)
+// when i click the <li>area i hope the icon got changed 
+//and get spired from css. the point is the state of checked 
+//but because we alternative the content checkbox to a icon, we need to add the state checked manually 
 ValueList =localStorage.getItem("items")?localStorage.getItem("items").split(','):[]
 
+function check(e){
+//console.log(this)
+//console.log(e)
+//console.log(e.target)
+console.log(e.target)
+e.target.matches('input')
+}
 
 // Initialize the list on page load
 document.addEventListener('DOMContentLoaded', initializeList);
 function initializeList(){
 if (ValueList.length>0){
-  Arts.innerHTML=ValueList.join('');
+ 
+  Arts.innerHTML=html;
 }  else {Arts.innerHTML= `<li>loading..........</li>`}
 }
 
@@ -26,31 +37,38 @@ reset.addEventListener('click',function(){
   ValueList=[]
 })  
 
-function submit(e){
+/* function submit(e){
   e.preventDefault()
- ValueList.push(` <li><input type='checkbox' > <label>${text.value}</label></li>`)
- console.log(ValueList)
+ ValueList.push(` <li><input type='checkbox' id='1' > <label for='1' >${text.value}</label></li>`)
+
+ //console.log(ValueList)
 
  Arts.innerHTML=ValueList.join('');
  localStorage.setItem("items", ValueList);
-
+ // the weakness of this methode is,: save the html with no checked valeu, 
+ //cause right now, we just add items, and the click event occurs later. 
+ //it's impossible to add the varible checked later in js, cause it's already in localstorage, 
+ //so we need to see state of check as a varible like ${text.value}
  addItems.reset()
-}
+} */
+
 
 
 // here we store the whole html in local then use ValueList to get the html and then just put it in Arts.innerHTML
  // actually whole html west the space, usually only keep value, and treat value as varible in html
  
-/* 
- ValueList=[]
+ 
+
  function xx(e){
   e.preventDefault()
 
   ValueList.push(text.value)
-  console.log(ValueList)
+ 
   localStorage.setItem("items", ValueList);
   ValueList =localStorage.getItem("items")?localStorage.getItem("items").split(','):[]
-  console.log(ValueList)
-  Arts.innerHTML=ValueList.map(value=>(`<li><input type='checkbox' > <label>${value}</label></li>`)).join('')
-  console.log( Arts.innerHTML)
-}*/
+  
+  let html=ValueList.map(value=>(`<li><input type='checkbox' > <label>${value}</label></li>`)).join('')
+  Arts.innerHTML=html;
+  console.log(html)
+  addItems.reset()
+}
